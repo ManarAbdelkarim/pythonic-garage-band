8051c67c9271a527937edb54c0e06bbba9e09cf0
ManarAbdelkarim <manarbdelkarim434@gmail.com>
2021-05-06 00:48:40 +0300

    solved

--- pythonic-garage-band/pythonic_garage_band/band.py
+++ pythonic-garage-band/pythonic_garage_band/band.py
@@ -2,10 +2,12 @@ from abc import abstractclassmethod
 
 class Musician:
     members = []
+    all_bands = []
     def __init__(self,name):
        
         self.name = name
         Musician.members.append(self)
+        self.__class__.all_bands.append(self)
 
 
 
@@ -87,16 +89,18 @@ class Band(Musician):
        
         self.name = name
         self.members = members
+        Band.instances.append(self.name)
+        
 
     def play_solos(self):
         solos = []
-        for member in Band.members:
+        for member in self.members:
             solos.append(member.play_solo())
         return solos
 
     @classmethod
     def to_list(cls):
-        return cls.members
+        return cls.bands
 
 
 
@@ -104,20 +108,77 @@ class Band(Musician):
 
 
 
-one_band = Band(
-        "Nirvana",
-        [Guitarist("Kurt Cobain"), Bassist("Krist Novoselic"), Drummer("Dave Grohl"),],
-    )
+# one_band = Band(
+#         "Nirvana",
+#         [Guitarist("Kurt Cobain"), Bassist("Krist Novoselic"), Drummer("Dave Grohl"),],
+#     )
 
 
+# two_band = Band("nobody",[])
+# print(two_band.to_list())
+# solos = one_band.play_solos()
+# print(solos) 
+# print(solos[0])
+# print(len(solos))
 
-solos = one_band.play_solos()
-print(solos) 
-print(solos[0])
-print(len(solos))
+
+class Musician():
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"My name is {self.name} and I play {self.instrument}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__} instance. Name = {self.name}"
+
+    def get_instrument(self):
+        return f"{self.instrument}"
+
+    def play_solo(self):
+        return f"{self.solo}"
 
 
+class Guitarist(Musician):
+    def __init__(self, name):
 
+        super().__init__(name)
+        self.instrument = "guitar"
+        self.solo = "face melting guitar solo"
+
+class Bassist(Musician):
+    def __init__(self, name):
+
+        super().__init__(name)
+        self.instrument = "bass"
+        self.solo = "bom bom buh bom"
+
+
+class Drummer(Musician):
+    def __init__(self, name):
+
+        super().__init__(name)
+        self.instrument = "drums"
+        self.solo = "rattle boom crash"
+
+
+class Band(Musician):
+    def __init__(self,name,members):
+       
+        self.name = name
+        self.members = members
+        Band.instances.append(self.name)
+        
+
+    def play_solos(self):
+        solos = []
+        for member in self.members:
+            solos.append(member.play_solo())
+        return solos
+
+    @classmethod
+    def to_list(cls):
+        return cls.instances
 
 
 
