ee65989260d0bfc0daf99ea901aac536d61fd898
ManarAbdelkarim <manarbdelkarim434@gmail.com>
2021-05-06 05:20:24 +0300

    solved

--- pythonic-garage-band/pythonic_garage_band/band.py
+++ pythonic-garage-band/pythonic_garage_band/band.py
@@ -1,86 +1,71 @@
+""" this is a module for lab 4 """
 from abc import abstractclassmethod
 
 
 class Musician:
-    # members = []
+    """
+    super class for musicians that takes name attribute
+    and contains the methods to be used by sub-classess
+    """
 
     def __init__(self, name):
 
         self.name = name
-        # Musician.members.append(self)
 
-
-    @abstractclassmethod
     def __str__(self):
-        pass
+        return "My name is {} and I play {}".format(self.name, self.instrument)
 
-    @abstractclassmethod
     def __repr__(self):
-        pass
+        return f"{self.__class__.__name__} instance. Name = {self.name}"
 
-    @abstractclassmethod
     def get_instrument(self):
-        pass
+        return f"{self.instrument}"
 
-    @abstractclassmethod
     def play_solo(self):
-        pass
+        return f"{self.solo}"
 
 
 class Guitarist(Musician):
-    def __str__(self):
+    """
+    sub class (child) of Musician that contains methods for Guitarist
+    """
 
-        return "My name is {} and I play guitar".format(self.name)
+    def __init__(self, name):
 
-    def __repr__(self):
-
-        return "Guitarist instance. Name = {}".format(self.name)
-
-    def get_instrument(self):
-        return "guitar"
-
-    def play_solo(self):
-        return "face melting guitar solo"
-
-
-# if __name__=="__main__":
-#     pass
+        super().__init__(name)
+        self.instrument = "guitar"
+        self.solo = "face melting guitar solo"
 
 
 class Drummer(Musician):
-    def __str__(self):
+    """
+    sub class (child) of Musician that contains methods for Drummer
+    """
 
-        return "My name is {} and I play drums".format(self.name)
-
-    def __repr__(self):
-
-        return "Drummer instance. Name = {}".format(self.name)
-
-    def get_instrument(self):
-        return "drums"
-
-    def play_solo(self):
-        return "rattle boom crash"
+    def __init__(self, name):
+        super().__init__(name)
+        self.instrument = "drums"
+        self.solo = "rattle boom crash"
 
 
 class Bassist(Musician):
-    def __str__(self):
+    """
+    sub class (child) of Musician that contains methods for Bassist
+    """
 
-        return "My name is {} and I play bass".format(self.name)
-
-    def __repr__(self):
-
-        return "Bassist instance. Name = {}".format(self.name)
-
-    def get_instrument(self):
-        return "bass"
-
-    def play_solo(self):
-        return "bom bom buh bom"
+    def __init__(self, name):
+        super().__init__(name)
+        self.instrument = "bass"
+        self.solo = "bom bom buh bom"
 
 
-class Band(Musician):
-    instances =[]
+class Band:
+    """
+    a class to create a group of musicians with the name of their band
+    """
+
+    instances = []
+
     def __init__(self, name, members):
         self.name = name
         self.members = members
@@ -94,18 +79,7 @@ class Band(Musician):
 
     @classmethod
     def to_list(cls):
+        """
+        a class to pass
+        """
         return cls.instances
-
-
-# one_band = Band(
-#         "Nirvana",
-#         [Guitarist("Kurt Cobain"), Bassist("Krist Novoselic"), Drummer("Dave Grohl"),],
-#     )
-
-
-# two_band = Band("nobody",[])
-# print(two_band.to_list())
-# solos = one_band.play_solos()
-# print(solos)
-# print(solos[0])
-# print(len(solos))
